70121e03e815679ccdf9851800874f9cffceb5b1
Ning <nche0024@student.monash.edu>
2020-04-11 23:56:43 +1000

    new

--- main/routes.js
+++ main/routes.js
@@ -1,9 +1,19 @@
 var express = require('express')
-
 var router = express.Router()
+var pool = require('./db')
+
+
+
 
 router.get('/hello', function(req, res){
     res.json('hello world')
+})
+
+
+router.get('/api/get/allposts', (req, res, next) => {
+    pool.query("SELECT * FROM posts", (q_err, q_res) =>{
+        res.json(q_res.rows)
+    })
 })
 
 module.exports = router
