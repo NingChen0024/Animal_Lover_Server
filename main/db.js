a8bd72fdd5c7049551c4177372325576dcf6d836
Ning <nche0024@student.monash.edu>
2020-04-15 15:43:44 +1000

    all

--- main/db.js
+++ main/db.js
@@ -1,11 +1,11 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'mydb',
-    password: 'abc1770017',
-    port: 5432
+    user: 'nche0024',
+    host: 'anidb.cihotuugslrg.ap-southeast-2.rds.amazonaws.com',
+    database: 'Anidb',
+    password: 'Abc1770017',
+    port: 5432, ssl = true
 })
 
 module.exports = pool
