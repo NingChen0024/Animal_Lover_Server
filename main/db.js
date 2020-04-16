1e62717b74d04372b474179fa891f5bef1af5d13
Ning <nche0024@student.monash.edu>
2020-04-16 16:51:17 +1000

    new

--- main/db.js
+++ main/db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     host: 'anidb.cihotuugslrg.ap-southeast-2.rds.amazonaws.com',
     database: 'Anidb',
     password: 'Abc1770017',
-    port: 5432, ssl = true
+    port: 5432, 
+
 })
 
 module.exports = pool
