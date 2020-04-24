27fac48e60264449f8999fd0fa00495c653eb965
Ning <nche0024@student.monash.edu>
2020-04-24 22:19:24 +1000

    new

---
+++ build/precache-manifest.d5b60f5c0bd16b4c24562c2f278a9ddf.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4824f4631778ae9955d68b4ac34575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "280ad37aada437c2998d",
+    "url": "/static/css/main.33ce7b5f.chunk.css"
+  },
+  {
+    "revision": "a33d2ae48725c2e1ffef",
+    "url": "/static/js/2.e3e84a3d.chunk.js"
+  },
+  {
+    "revision": "621da2fc7278363160e2a5855b9a0c01",
+    "url": "/static/js/2.e3e84a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280ad37aada437c2998d",
+    "url": "/static/js/main.fadbdb1c.chunk.js"
+  },
+  {
+    "revision": "8d95edaf1a05585320ac",
+    "url": "/static/js/runtime-main.2b651237.js"
+  },
+  {
+    "revision": "a9c7f5b9e6fd5a10530045746132da38",
+    "url": "/static/media/Kids_tips.a9c7f5b9.jpg"
+  },
+  {
+    "revision": "bb3f981c13f6106fdd2da30465ccce63",
+    "url": "/static/media/Plain-Bold.bb3f981c.woff2"
+  },
+  {
+    "revision": "c2491f77feb35c8c76322f4805dfd012",
+    "url": "/static/media/Plain-Bold.c2491f77.woff"
+  },
+  {
+    "revision": "3284585d91b7e8497fd8d8a643032763",
+    "url": "/static/media/Plain-Light.3284585d.woff2"
+  },
+  {
+    "revision": "3c3eb89fb2ea963546dcf3cef04e3e57",
+    "url": "/static/media/Plain-Light.3c3eb89f.woff"
+  },
+  {
+    "revision": "6b59fff5e6decdedca58eb84e2781029",
+    "url": "/static/media/Plain-Regular.6b59fff5.woff"
+  },
+  {
+    "revision": "f2c7841a8e557a24d89db8c1f8a02f20",
+    "url": "/static/media/Plain-Regular.f2c7841a.woff2"
+  },
+  {
+    "revision": "ab69b49cfd30e02c15aa7628d5655e36",
+    "url": "/static/media/Project_one.ab69b49c.jpg"
+  },
+  {
+    "revision": "522f9508fb40a34a0d23e575cb6fa62f",
+    "url": "/static/media/Project_three.522f9508.jpg"
+  },
+  {
+    "revision": "56a88f1a08bf8f66c3da3f014080f19a",
+    "url": "/static/media/Project_two.56a88f1a.jpg"
+  },
+  {
+    "revision": "0e52ca8622ad78cb302409843790dd05",
+    "url": "/static/media/Wombat_cartoon.0e52ca86.gif"
+  },
+  {
+    "revision": "bb68dd39107d1b65ea1d252376f0c3ec",
+    "url": "/static/media/faq_bear.bb68dd39.png"
+  },
+  {
+    "revision": "79bca161ed384d97794f5ec339da6970",
+    "url": "/static/media/faq_buble.79bca161.jpg"
+  },
+  {
+    "revision": "7e9f1c2335b657b630bc7e6a707dd064",
+    "url": "/static/media/faq_dog.7e9f1c23.png"
+  },
+  {
+    "revision": "322a1eb21c5781b38e413e9854ba77ab",
+    "url": "/static/media/houses.322a1eb2.jpg"
+  },
+  {
+    "revision": "2802c72a767242dcf4365464a98d9cde",
+    "url": "/static/media/kangaroo_cartoon.2802c72a.png"
+  },
+  {
+    "revision": "d86bf781615b9838d541a64641f6bb1b",
+    "url": "/static/media/kids-tip1.d86bf781.png"
+  },
+  {
+    "revision": "8aa916802404e4242ee7e32ca3371a08",
+    "url": "/static/media/kids-tip2.8aa91680.jpg"
+  },
+  {
+    "revision": "e7a11e53f920822bda052c344d1cc320",
+    "url": "/static/media/kids-tip3.e7a11e53.jpg"
+  },
+  {
+    "revision": "b0dc9a2df94595b135e3e092a091a311",
+    "url": "/static/media/koala_cartoon.b0dc9a2d.png"
+  },
+  {
+    "revision": "61da23b5527032bde8e6331a0a13523f",
+    "url": "/static/media/koala_icon.61da23b5.png"
+  },
+  {
+    "revision": "d8dbe379d3d9f04fe04884d076e6c9fa",
+    "url": "/static/media/koala_pho.d8dbe379.jpg"
+  },
+  {
+    "revision": "39b7d39ca60f0fa50dcb7261d654603b",
+    "url": "/static/media/newsletter.39b7d39c.png"
+  },
+  {
+    "revision": "02fee2d081e7a623cddbf133fa6f0c0c",
+    "url": "/static/media/parents_cons.02fee2d0.jpg"
+  },
+  {
+    "revision": "d27da7401cddf8aff624965724c8a898",
+    "url": "/static/media/parents_faq.d27da740.jpg"
+  },
+  {
+    "revision": "536b76fdae8194e97844587052bd05b2",
+    "url": "/static/media/parents_help.536b76fd.jpg"
+  },
+  {
+    "revision": "23d21ddf53ae3266f325d018c1d3d3b1",
+    "url": "/static/media/parents_tips.23d21ddf.jpg"
+  },
+  {
+    "revision": "424547a9e2d90043ca06a522e1890323",
+    "url": "/static/media/wallaby_icon.424547a9.png"
+  },
+  {
+    "revision": "cdc57de96b9d9a58831a8db3076ebab5",
+    "url": "/static/media/wallaby_pho.cdc57de9.jpg"
+  },
+  {
+    "revision": "86ea78e3593f8bc5d914e4c643b16698",
+    "url": "/static/media/wombat_icon.86ea78e3.png"
+  },
+  {
+    "revision": "75af5d1cf7dac8ffde82ba84686dda2f",
+    "url": "/static/media/wombat_pho.75af5d1c.jpg"
+  }
+]);
