ca57a2247b943d14c9e38d1a1be1eccffff1f119
Ning <nche0024@student.monash.edu>
2020-04-24 22:53:32 +1000

    new

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/', indexRouter);
 
 if (true){
     app.use(express.static('build'))
-    app.get('/*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 }
