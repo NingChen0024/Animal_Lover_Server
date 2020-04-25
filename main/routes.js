c258e554f3fa837fd73c0c7581319761af749e34
Ning <nche0024@student.monash.edu>
2020-04-25 15:11:25 +1000

    ew

--- main/routes.js
+++ main/routes.js
@@ -9,8 +9,10 @@ router.get('/hello', function(req, res){
     res.json('hello world')
 })
 
+
+
 router.get('/api/get/allanimals', (req, res, next) => {
-    pool.query("SELECT * FROM animals", (q_err, q_res) =>{
+    pool.query("SELECT * FROM users", (q_err, q_res) =>{
         res.json(q_res.rows)
     })
 })
