f5d863e4a020008f2fdf098be9c59259926cb890
Ning <nche0024@student.monash.edu>
2020-05-11 15:52:20 +1000

    new

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2e18b38ae4ade31a6863fa869486e76.js"
+  "/precache-manifest.0c3082af81de7a622955d1137a6d3e9c.js"
 );
 
 self.addEventListener('message', (event) => {
